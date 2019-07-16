8d6b9fbfe0a97edf3e5767f06496a7f0cec232bd
Ubuntu <ubuntu@ip-172-31-83-176.ec2.internal>
2019-07-16 16:07:46 +0000

    impliment basic routes & views

--- run.py
+++ run.py
@@ -1,13 +1,23 @@
 import os
 from flask import Flask
 
-# app initialised
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello There</h1>"
+    """Main page with instructions"""
+    return "To send a message use: /USERNAME/MESSAGE"
+
+# if we type /Paraic into the url then the message "Hi Paraic" is returned on screen
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+# if we type /Paraic/thisMessage into the url then "Paraic: thisMessage" is returned on screen
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
